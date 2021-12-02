ecf59c7bd2ee727c72aea7c5aaf077d4395117f7
Maurizio Davoli <davolimau@gmail.com>
2021-12-02 11:14:02 +0100

    Update test_actions.py

--- test/test_actions.py
+++ test/test_actions.py
@@ -10,16 +10,21 @@ def read_from_s3():
     print("prima")
     s3_client = boto3.client('s3')
 
-    with open("document.bin", "wb") as file:
-        s3_client.download_file('test-translated-bucket', '1.txt', 'document.bin')
-        file.close()
-
-    with open("document.bin", "rb") as file2:
-        cont = file2.read().decode("ascii")
-        file2.close()
-    os.remove("document.bin")
-    print(cont)
-    print("dopo")
+    check_over = False
+    id_obj = 1
+    while not check_over:
+        try:
+            with open("document.bin", "wb") as file:
+                s3_client.download_file('test-translated-bucket', '1.txt', 'document.bin')
+                file.close()
+            with open("document.bin", "rb") as file2:
+                cont = file2.read().decode("ascii")
+                file2.close()
+            os.remove("document.bin")
+            print(cont)
+            id_obj = id_obj + 1
+        except TypeError:
+            check_over = True
 
 
 read_from_s3()
