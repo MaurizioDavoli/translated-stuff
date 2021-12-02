2c10af3d425d92d1e48748b635b6c78d61501145
Maurizio Davoli <davolimau@gmail.com>
2021-12-02 10:46:36 +0100

    Update test_actions.py

--- test/test_actions.py
+++ test/test_actions.py
@@ -1,1 +1,14 @@
+"""testing actions script"""
+import boto3
+
 print("I ran thanks to a GitHub action")
+
+
+def read_from_s3():
+    """read a random file from s3"""
+    print("prima")
+    s3_client = boto3.client('s3')
+    print("dopo")
+
+
+read_from_s3()
