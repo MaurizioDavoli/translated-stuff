f4402e0bc6172220ea0f3066af96a99095ff2b40
Maurizio Davoli <davolimau@gmail.com>
2021-12-03 14:07:04 +0100

    Update test_conversation_of.py

--- test/test_conversation_of.py
+++ test/test_conversation_of.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 
-from front.get_message_from_date import get_conversations_of
+from front import get_message_from_date as to_test
 
+i
 
 def test_date_accuracy():
     date = datetime(2021, 10, 1)
-    loaded_data_frame = get_conversations_of(date)
+    loaded_data_frame = to_test.get_conversations_of(date)
     print(loaded_data_frame)
 
 
